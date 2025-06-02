be048ab18a7091d7e3833fdcf8b41d9d15ca02a0
AlvaroAlonsoDev <airuritac@gmail.com>
2025-06-02 18:24:33 +0200

    init PWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,35 @@
+// vite.config.ts
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'MeetBack',
+        short_name: 'MeetBack',
+        description: 'Conecta. Opina. Mejora.',
+        theme_color: '#1e3a8a', // Azul oscuro
+        background_color: '#1e3a8a',
+        icons: [
+          {
+            src: '/icon-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ],
+        display: 'standalone',
+        start_url: '/',
+        lang: 'es'
+      }
+    })
+  ]
 })
