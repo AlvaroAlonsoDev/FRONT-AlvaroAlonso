769a1de40d992f5a5f8523938afdbe82ffa842ab
AlvaroAlonsoDev <airuritac@gmail.com>
2025-06-02 21:59:31 +0200

    Delete navBar

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { AuthProvider } from './contexts/AuthContext'
-import NavBar from './components/NavBar'
 import Profile from './pages/Profile'
 import Feed from './pages/Feed'
 import Login from './pages/Login'
