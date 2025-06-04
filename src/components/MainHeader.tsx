b4f6e0664a5a862ef4261ad11fe9abd3e494c5bf
AlvaroAlonsoDev <airuritac@gmail.com>
2025-06-04 17:02:10 +0200

    vercel fix

--- src/components/MainHeader.tsx
+++ src/components/MainHeader.tsx
@@ -1,7 +1,6 @@
 import { Link, useNavigate } from "react-router-dom";
 import { useAuth } from "../contexts/AuthContext";
-import { CiChat1 } from "react-icons/ci";
-import { MessageCircle, Text } from "lucide-react";
+import { MessageCircle } from "lucide-react";
 
 export function MainHeader() {
     const navigate = useNavigate();
